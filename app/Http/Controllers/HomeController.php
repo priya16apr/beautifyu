74c9478ee431adcd19365968e5d6b54c555348dd
DESKTOP-A6617OS\dell <priya.16apr@gmail.com>
2024-04-13 21:59:19 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $p_arrival      =  Product::where('status','4')->orderby('id','desc')->paginate('12');
         $p_trending     =  Product::where('status','4')->orderby('id','desc')->paginate('12');
         $p_celeb        =  Product::where('status','4')->where('is_celeb','Yes')->paginate('4');
-        $p_combo        =  Product::where('status','4')->paginate('4');
+        $p_combo        =  Product::where('status','4')->where('producttype_id','7')->paginate('4');
         $p_deal         =  Product::where('status','4')->paginate('4');
        
         $probanner      =  HomeuiBanner::where('type','Promotional Banner')->where('status','Active')->get();
