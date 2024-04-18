1dde194b6140dbfc7f4b123dc1bbbd50233c90ac
DESKTOP-A6617OS\dell <priya.16apr@gmail.com>
2024-04-18 12:03:33 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Session;
 use App\Models\HomeuiBanner;
 use App\Models\HomeuiUsp;
 use App\Models\Product;
+use App\Models\HappyCustomer;
 
 class HomeController extends Controller
 {
@@ -22,9 +23,9 @@ class HomeController extends Controller
         $p_combo        =  Product::where('status','4')->where('producttype_id','7')->paginate('4');
         $p_deal         =  Product::where('status','4')->paginate('4');
        
-        $probanner      =  HomeuiBanner::where('type','Promotional Banner')->where('status','Active')->get();
+        $hcustomer      =  HappyCustomer::where('is_visible','Yes')->orderby('id','desc')->get();
         
-        $info           =  compact('topbanner','usp','p_arrival','p_trending','p_celeb','p_combo','p_deal','probanner');
+        $info           =  compact('topbanner','usp','p_arrival','p_trending','p_celeb','p_combo','p_deal','hcustomer');
         
         return view('index')->with($info);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -204,60 +204,19 @@
             <div class="container-fluid">
                 <h3>Our Happy Customers</h3><br />
                 <div class="row">
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-5.jpg">
-                            <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
+                    @if(count($hcustomer)>0)
+                        @foreach($hcustomer as $key=>$hcustomers)
+                            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
+                                <div class="instagram__item set-bg" data-setbg="{{$hcustomers->image}}">
+                                    <div class="instagram__text">
+                                        <img src="{{$hcustomers->product->default_img}}">
+                                        <p>Rs. {{$hcustomers->product->selling_price}}</p>
+                                        <a href="product/{{$hcustomers->product->slug}}">View product</a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-6.jpg">
-                        <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-5.jpg">
-                        <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-6.jpg">
-                        <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-5.jpg">
-                        <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/product/product-6.jpg">
-                        <div class="instagram__text">
-                                <img src="img/product/product-7.jpg">
-                                <p>Rs. 300</p>
-                                <a href="#">View product</a>
-                            </div>
-                        </div>
-                    </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
