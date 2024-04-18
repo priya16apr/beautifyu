47b3d8e618536355ec33c7435e45080683ce0c09
DESKTOP-A6617OS\dell <priya.16apr@gmail.com>
2024-04-18 12:10:32 +0530

    changes

--- app/Http/Controllers/HappyCustomerController.php
+++ app/Http/Controllers/HappyCustomerController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Content;
+use App\Models\HappyCustomer;
 
 class HappyCustomerController extends Controller
 {
     public function index()
     {
-        return view('happycustomer.index');
+        $hcustomer      =  HappyCustomer::where('is_visible','Yes')->orderby('id','desc')->get();
+
+        $info           =  compact('hcustomer');
+        
+        return view('happycustomer.index')->with($info);
     }
 
 }
--- resources/views/happycustomer/index.blade.php
+++ resources/views/happycustomer/index.blade.php
@@ -6,7 +6,28 @@
 
 @section('mid-content')
 
-    Happy Customer Page
+    <section>
+        <div class="instagram">
+            <div class="container-fluid">
+                <h3>Our Happy Customers</h3><br />
+                <div class="row">
+                    @if(count($hcustomer)>0)
+                        @foreach($hcustomer as $key=>$hcustomers)
+                            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
+                                <div class="instagram__item set-bg" data-setbg="{{$hcustomers->image}}">
+                                    <div class="instagram__text">
+                                        <img src="{{$hcustomers->product->default_img}}">
+                                        <p>Rs. {{$hcustomers->product->selling_price}}</p>
+                                        <a href="product/{{$hcustomers->product->slug}}">View product</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
 
